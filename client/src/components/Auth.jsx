92378af170cb93034e4e07eae207a510efee101f
Julian Swerdlin <julianswer36@gmail.com>
2022-01-27 00:51:40 +0100

    uploading

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,35 +1,46 @@
-import React, {useState} from 'react';
-import Cookies from 'universal-cookie';
-import axios from 'axios'
+import React, { useState } from "react";
+import Cookies from "universal-cookie";
+import axios from "axios";
 
-import signInImage from '../assets/signup.jpg'
+import signInImage from "../assets/signup.jpg";
 
 const Auth = () => {
+  const [isSignUp, setIsSignUp] = useState(true);
 
-    const [isSignUp, setIsSignUp] = useState(true);
-
-    const handleChange = () => {}
+  const handleChange = () => {};
 
   return (
-      <div className="auth__form-container">
-          <div className="auth__form-container_fields">
-            <div className="auth__form-container_fields-content">
-                <p>{isSignUp ? 'Sign Up' : 'Sign In'}</p>
-                <form onSubmit={{}}>
-                    {isSignUp && (
-                        <div className="auth__form-container_fields-content_input">
-                            <label htmlFor="fullName">Full Name</label>    
-                            <input type="text" name="fullName" placeholder="Full Name" onchange={handleChange} required/>
-                        </div>
-                    )}
-                    <div className="auth__form-container_fields-content_input">
-                        <label htmlFor="username">Username</label>    
-                        <input type="text" name="username" placeholder="Username" onchange={handleChange} required/>
-                    </div>
-                </form>
+    <div className="auth__form-container">
+      <div className="auth__form-container_fields">
+        <div className="auth__form-container_fields-content">
+          <p>{isSignUp ? "Sign Up" : "Sign In"}</p>
+          <form onSubmit={{}}>
+            {isSignUp && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="fullName">Full Name</label>
+                <input
+                  type="text"
+                  name="fullName"
+                  placeholder="Full Name"
+                  onchange={handleChange}
+                  required
+                />
+              </div>
+            )}
+            <div className="auth__form-container_fields-content_input">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                onchange={handleChange}
+                required
+              />
             </div>
-          </div>
+          </form>
+        </div>
       </div>
+    </div>
   );
 };
 
