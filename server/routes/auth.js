cd46409692370179ed4415eef32b00937f2e7248
Julian Swerdlin <julianswer36@gmail.com>
2022-01-27 17:38:37 +0100

    updating controllers from the backend

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,4 +4,6 @@ const {signup, login} = require('../controllers/auth')
 const router = express.Router();
 
 router.post('/signup', signup);
-router.post('/login', login)
+router.post('/login', login);
+
+module.exports = router;
