661f0775440a5dbb9d4b950a2eaa725338d1e516
Julian Swerdlin <julianswer36@gmail.com>
2022-01-30 23:34:33 +0100

    new changes

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import {useChatContext} from 'stream-chat-react';
+import {UserList} from './';
+import {CloseCreateChannel} from '../assets'
+
+const ChannelNameInput = ({channelName = '', setChannelName}) => {
+  return (
+    <div className="channel-name-input__wrapper">
+      <p>Name</p>
+      <input value={} onChange={} placeholder="channel-name (no spaces)" />
+    </div>
+  )
+}
 
 const CreateChannel = () => {
-  return <div>Create Channel</div>;
+  return (
+    <div>
+      create channel
+    </div>
+  );
 };
 
 export default CreateChannel;
